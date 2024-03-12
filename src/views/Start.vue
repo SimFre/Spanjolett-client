85e81b38b46ec9648c6bbf8216ae3ca9b04af026
Simon Fredriksson <simon@lan2k.org>
2024-03-12 16:56:41 +0100

    Add QR code generator

--- src/views/Start.vue
+++ src/views/Start.vue
@@ -1,13 +1,21 @@
 <script setup>
 import { ref, computed } from "vue";
 import { useConfigStore } from "@/pinia/ConfigStore.js";
+import QrcodeVue from "qrcode.vue";
+
 const config = useConfigStore();
+const data = "https://dn.se";
 </script>
 
 <template>
   <div className="grid">
     <div className="col-12">
       <div className="card">
+        <QrcodeVue
+          :value="data"
+          size="300"
+          level="H"
+        />
         <hr />
         Config:
         <samp>{{ config }}</samp>
